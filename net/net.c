c2e1cb01170d7ff40dde932cd6b2f4d95dc46356
Chris Packham <judge.packham@gmail.com>
2015-11-04 09:25:13 +1300

    net: IPv6 support
    Adds basic support for IPv6. Neighbor discovery and ping6 are the only
    things supported at the moment.
    
    Helped-by: Hanna Hawa <hannah@marvell.com> [endian & alignment fixes]
    Signed-off-by: Chris Packham <judge.packham@gmail.com>
    Patch-cc: Simon Glass <sjg@chromium.org>
    
    Series-changes: 2
    - split ping6 support into separate patch
    - split environment variables into separate patch
    - change ip6_ndisc_* to ndisc_*, fix CamelCase
    
    Commit-notes:
    Now we have something functional. With this and the next patch you can
    do something like 'setenv ipaddr6 3ffe::1/64' and 'ping6 3ffe::2' should
    work.
    
    I seem to have a problem that when you send a ping6 for a non-existent
    address that ends up stuck and the next non-ipv6 net operation tries to
    resolve it. I suspect this is because the pending neighbor discovery
    information isn't cleaned up properly, I need to look into that.
    END

--- net/net.c
+++ net/net.c
@@ -96,6 +96,7 @@
 #include <image.h>
 #include <net.h>
 #include <net/fastboot.h>
+#include <net6.h>
 #include <net/tftp.h>
 #if defined(CONFIG_CMD_PCAP)
 #include <net/pcap.h>
@@ -107,6 +108,7 @@
 #include <watchdog.h>
 #include <linux/compiler.h>
 #include "arp.h"
+#include "ndisc.h"
 #include "bootp.h"
 #include "cdp.h"
 #if defined(CONFIG_CMD_DNS)
@@ -353,8 +355,12 @@ void net_auto_load(void)
 
 static void net_init_loop(void)
 {
-	if (eth_get_dev())
+	if (eth_get_dev()) {
 		memcpy(net_ethaddr, eth_get_ethaddr(), 6);
+#ifdef CONFIG_NET6
+		ip6_make_lladdr(&net_link_local_ip6, net_ethaddr);
+#endif
+	}
 
 	return;
 }
@@ -388,6 +394,9 @@ void net_init(void)
 				(i + 1) * PKTSIZE_ALIGN;
 		}
 		arp_init();
+#ifdef CONFIG_NET6
+		ndisc_init();
+#endif
 		net_clear_handlers();
 
 		/* Only need to setup buffer pointers once. */
@@ -497,6 +506,11 @@ restart:
 			ping_start();
 			break;
 #endif
+#ifdef CONFIG_CMD_PING6
+		case PING6:
+			ping6_start();
+			break;
+#endif
 #if defined(CONFIG_CMD_NFS) && !defined(CONFIG_SPL_BUILD)
 		case NFS:
 			nfs_start();
@@ -564,6 +578,11 @@ restart:
 		WATCHDOG_RESET();
 		if (arp_timeout_check() > 0)
 			time_start = get_timer(0);
+#ifdef CONFIG_NET6
+		else if (ndisc_timeout_check() > 0)
+			time_start = get_timer(0);
+#endif
+
 
 		/*
 		 *	Check the ethernet for a new packet.  The ethernet
@@ -579,6 +598,9 @@ restart:
 		if (ctrlc()) {
 			/* cancel any ARP that may not have completed */
 			net_arp_wait_packet_ip.s_addr = 0;
+#ifdef CONFIG_NET6
+			net_nd_sol_packet_ip6 = net_null_addr_ip6;
+#endif
 
 			net_cleanup_loop();
 			eth_halt();
@@ -1187,6 +1209,11 @@ void net_process_received_packet(uchar *in_packet, int len)
 		rarp_receive(ip, len);
 		break;
 #endif
+#ifdef CONFIG_NET6
+	case PROT_IP6:
+		net_ip6_handler(et, (struct ip6_hdr *)ip, len);
+		break;
+#endif
 	case PROT_IP:
 		debug_cond(DEBUG_NET_PKT, "Got IP\n");
 		/* Before we start poking the header, make sure it is there */
@@ -1335,6 +1362,14 @@ static int net_check_prereq(enum proto_t protocol)
 #if defined(CONFIG_CMD_PING)
 	case PING:
 		if (net_ping_ip.s_addr == 0) {
+			puts("*** ERROR: ping address not given\n");
+			return 1;
+		}
+		goto common;
+#endif
+#ifdef CONFIG_CMD_PING6
+	case PING6:
+		if (ip6_is_unspecified_addr(&net_ping_ip6)) {
 			puts("*** ERROR: ping address not given\n");
 			return 1;
 		}
